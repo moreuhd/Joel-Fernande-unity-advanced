31f5157d00b25c7e66b391065623e25eaeea3be2
moreuhd <joel_fernandes@eticalgarve.com>
2025-06-17 16:50:44 +0100

    Update Door.cs

--- JoelFernandes_Projeto_FPS/Assets/Scripts/Door.cs
+++ JoelFernandes_Projeto_FPS/Assets/Scripts/Door.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class Door : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Animator animator;
+    private void OnTriggerEnter(Collider other)
     {
-        
+        RenderSettings.fog = !RenderSettings.fog;
+        animator.SetTrigger("Open");
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
